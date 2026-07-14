1173c11ca135a9cf8c1f9f630828087e3eca207e
branefungi <benjaminzeng3@gmail.com>
2026-07-14 10:29:49 -0400

    Create Teleop.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Teleop", group = "TeleOp")
+public class Teleop extends LinearOpMode{
+
+    @Override
+    public void runOpMode() {
+
+
+
+        
+
+
+
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+
+
+
+
+        }
+    }
+}
