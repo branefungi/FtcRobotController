6c1d70245e4e192478a517937c85c265416d036a
branefungi <benjaminzeng3@gmail.com>
2026-07-14 12:29:48 -0400

    Update Auto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,1 +1,44 @@
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "Auto", group = "Auto")
+public class Auto extends LinearOpMode{
+
+    @Override
+    public void runOpMode() {
+
+        // create a timer
+        ElapsedTime myTimer = new ElapsedTime();
+
+        waitForStart();
+
+        // pause the program, every 1000 is a 1 second pause
+        sleep(1000);
+        
+        
+
+    }
+    private void forward(){
+
+    }
+    private void backward(){
+
+    }
+    private void right(){
+
+    }
+    private void left(){
+
+    }
+    private void turnLeft(){
+
+    }
+    private void turnRight(){
+
+    }
+}
