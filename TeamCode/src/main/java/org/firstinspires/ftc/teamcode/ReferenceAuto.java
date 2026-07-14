376b41bf82067a90e1aef39e1d62cd63c168d8fb
branefungi <benjaminzeng3@gmail.com>
2026-07-14 12:37:19 -0400

    Update ReferenceAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ReferenceAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ReferenceAuto.java
@@ -1,11 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Disabled
 public class ReferenceAuto extends LinearOpMode{
@@ -13,21 +12,39 @@ public class ReferenceAuto extends LinearOpMode{
     @Override
     public void runOpMode() {
 
-        // Setup a motor
-        // Change "motorName" to match the name of the motor (FL, FR, BL, or BR)
-        DcMotor motorName;
-        motorName = hardwareMap.get(DcMotor.class, "motorName");
-        motorName.setDirection(DcMotorSimple.Direction.FORWARD); //
+        // create a timer
+        ElapsedTime myTimer = new ElapsedTime();
 
-        waitForStart();
+        // make the timer start counting up from 0
+        myTimer.reset();
 
-        while(opModeIsActive()){
+        // a double containing how much time the timer has measured
+        myTimer.seconds();
+
+        
+        
+        // pause the program, every 1000 is a 1 second pause
+        sleep(1000);
+
+
+        // set up a sensor the track the robot's angle
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        imu.initialize(new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        // the next two lines should match the control hub's orientation
+                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+                )
+        ));
+
+        // reset the measured angle to 0 degrees
+        imu.resetYaw();
+
+        // a double containing the angle the robot is facing 
+        Math.toDegrees(imu.getRobotYawPitchRollAngles().getYaw());
 
 
 
-          
-        }
 
-    
     }
 }
